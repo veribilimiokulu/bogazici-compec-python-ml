33d69c5ab535a17c87380c25cbd807949e076e36
Erkan ŞİRİN <erkansirin78@hotmail.com>
2019-03-15 12:12:18 +0300

    ilk yükleme

---
+++ 03_input_kullanicidan_bilgi_almak_odev.py
@@ -0,0 +1,5 @@
+sayi1 = input("1. sayıyı giriniz:")
+sayi2 = input("2. sayıyı giriniz:")
+print("\n")
+print(int(sayi1)," ve ", int(sayi1), "sayılarının toplamı:", int(sayi1) + int(sayi2))
+print("\n")
